8aa6d9692f28cfddd1d1c51dbd044752b100c1d1
Chris Ahlstrom <ahlstromcj@gmail.com>
2021-01-10 10:27:12 -0500

    Merged master, minor version updates.

--- include/qt/portmidi/seq66-config.h
+++ include/qt/portmidi/seq66-config.h
@@ -33,7 +33,7 @@
  * \library       seq66 application
  * \author        Chris Ahlstrom
  * \date          2018-11-10
- * \updates       2021-01-05
+ * \updates       2021-01-10
  * \license       GNU GPLv2 or above
  *
  *  Qt Portmidi Linux and Windows versions, hardwired for use with
@@ -63,7 +63,7 @@
 #endif
 
 #ifndef SEQ66_VERSION_DATE_SHORT
-#define SEQ66_VERSION_DATE_SHORT "2021-01-05"
+#define SEQ66_VERSION_DATE_SHORT "2021-01-10"
 #endif
 
 #ifndef SEQ66_VERSION
