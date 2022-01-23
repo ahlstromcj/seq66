9cbb9da9d6adc606fc5cd24a5273ea95f2f974c5
Chris Ahlstrom <ahlstromcj@gmail.com>
2022-01-23 14:04:21 -0500

    Stamped for next version, 0.98.4.

--- include/qt/portmidi/seq66-config.h
+++ include/qt/portmidi/seq66-config.h
@@ -66,7 +66,7 @@
 #endif
 
 #if ! defined SEQ66_VERSION
-#define SEQ66_VERSION "0.98.3"
+#define SEQ66_VERSION "0.98.4"
 #endif
 
 #if ! defined SEQ66_GIT_VERSION
@@ -325,7 +325,7 @@
 
 /* Define to the full name and version of this package. */
 #if ! defined SEQ66_PACKAGE_STRING
-#define SEQ66_PACKAGE_STRING "Seq66 0.98.3"
+#define SEQ66_PACKAGE_STRING "Seq66 0.98.4"
 #endif
 
 /* Define to the one symbol short name of this package. */
