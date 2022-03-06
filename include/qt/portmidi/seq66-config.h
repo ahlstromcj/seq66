2435ee6b95ac608cc67798882c8c9a00f4d3a2cc
Chris Ahlstrom <ahlstromcj@gmail.com>
2022-03-06 11:13:16 -0500

    Version date-stamp.

--- include/qt/portmidi/seq66-config.h
+++ include/qt/portmidi/seq66-config.h
@@ -33,7 +33,7 @@
  * \library       seq66 application
  * \author        Chris Ahlstrom
  * \date          2018-11-10
- * \updates       2022-02-17
+ * \updates       2022-03-06
  * \license       GNU GPLv2 or above
  *
  *  Qt Portmidi Linux and Windows versions, hardwired for use with
@@ -62,7 +62,7 @@
 #endif
 
 #if ! defined SEQ66_VERSION_DATE_SHORT
-#define SEQ66_VERSION_DATE_SHORT "2022-02-17"
+#define SEQ66_VERSION_DATE_SHORT "2022-03-06"
 #endif
 
 #if ! defined SEQ66_VERSION
