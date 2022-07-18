d55f7e46b9d7ee7ecbafabee9e75f2affdd3bff8
Chris Ahlstrom <ahlstromcj@gmail.com>
2022-07-18 11:24:47 -0400

    Version 0.98.10 pending.

--- include/qt/rtmidi/seq66-config.h
+++ include/qt/rtmidi/seq66-config.h
@@ -33,7 +33,7 @@
  * \library       seq66 application
  * \author        Chris Ahlstrom
  * \date          2020-05-31
- * \updates       2022-06-28
+ * \updates       2022-07-18
  * \license       GNU GPLv2 or above
  *
  *  Qt Rtmidi Linux and Windows versions, hardwired for use with
@@ -70,7 +70,7 @@
 #endif
 
 #if ! defined SEQ66_VERSION_DATE_SHORT
-#define SEQ66_VERSION_DATE_SHORT "2022-06-28"
+#define SEQ66_VERSION_DATE_SHORT "2022-07-18"
 #endif
 
 #if ! defined SEQ66_API_VERSION
