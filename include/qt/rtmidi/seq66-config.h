a6659a2409931e93aaef7bef49f367d717fb4216
Chris Ahlstrom <ahlstromcj@gmail.com>
2023-04-30 11:59:21 -0400

    Initial version bump.

--- include/qt/rtmidi/seq66-config.h
+++ include/qt/rtmidi/seq66-config.h
@@ -78,7 +78,7 @@
 #endif
 
 #if ! defined SEQ66_VERSION
-#define SEQ66_VERSION "0.99.4"
+#define SEQ66_VERSION "0.99.5"
 #endif
 
 #if ! defined SEQ66_GIT_VERSION
@@ -380,7 +380,7 @@
 
 /* Define to the full name and version of this package. */
 #if ! defined SEQ66_PACKAGE_STRING
-#define SEQ66_PACKAGE_STRING "Seq66 0.99.4"
+#define SEQ66_PACKAGE_STRING "Seq66 0.99.5"
 #endif
 
 /* Define to the one symbol short name of this package. */
