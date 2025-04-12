79585636bdc44eb276439f0ad465bc780afc8028
Chris <ahlstromcj@gmail.com>
2025-04-12 13:15:02 -0400

    Updating dates for local install/testing.

--- include/qt/rtmidi/seq66-config.h
+++ include/qt/rtmidi/seq66-config.h
@@ -33,7 +33,7 @@
  * \library       seq66 application
  * \author        Chris Ahlstrom
  * \date          2020-05-31
- * \updates       2025-03-02
+ * \updates       2025-04-12
  * \license       GNU GPLv2 or above
  *
  *  Qt Rtmidi Linux and Windows versions, hardwired for use with
@@ -70,7 +70,7 @@
 #endif
 
 #if ! defined SEQ66_VERSION_DATE_SHORT
-#define SEQ66_VERSION_DATE_SHORT "2025-03-02"
+#define SEQ66_VERSION_DATE_SHORT "2025-04-12"
 #endif
 
 #if ! defined SEQ66_API_VERSION
--- nsis/Seq66Constants.nsh
+++ nsis/Seq66Constants.nsh
@@ -3,7 +3,7 @@
 ; File:         Seq66Constants.nsh
 ; Author:       Chris Ahlstrom
 ; Date:         2018-05-26
-; Updated:      2025-03-02
+; Updated:      2025-04-12
 ; Version:      0.99.20
 ;
 ;   Provides constants commonly used by the installer for Seq66 for
